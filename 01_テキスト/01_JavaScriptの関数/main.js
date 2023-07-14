9706a99b22aec8712ff2b6902cf62b32eeccbc36
fjswhikaru <hikarufjswred@icloud.com>
2023-07-14 18:39:20 +0900

    [3の倍数]を学習しました。

--- 01_テキスト/01_JavaScriptの関数/main.js
+++ 01_テキスト/01_JavaScriptの関数/main.js
@@ -2,18 +2,53 @@
 const plus1 = function (x) {
   return x + 1
 }
-// 関数名:
-// 引数:
-// 返り値:
+// 関数名: plus1
+// 引数: x
+// 返り値: x + 1
 
 // 練習問題 ２
 // f1: 1, 4, 7, 10, 13, 16, ...
+//3n-2
+
+const f1 = function (n) {
+  return 3 * n + 1
+}
+
+//違う　nは0スタートだ
 
 // f2: 1, 10, 100, 1000, 10000, ...
+//10**n-1
+
+const f2 = function (n) {
+  return 10 ** n
+}
 
 // f3: 2, 14, 107, 1010, 10013, ...
+
+const f3 = function (n) {
+  return f1(n) + f2(n)
+}
 
 // 練習問題３
 // ① ２つの引数を受け取り、それらをかけ合わせた値を返す関数 multiply を作成して正しく動くかテストしてください。
 
+const multiply = function () {
+  return 6 * 7
+}
+
+//え、x y で考えるの？？？なんで上野じゃダメなの？？？→functionに引数がないからか？？
+
+const multiply = function (x, y) {
+  return x * y
+}
+
 // ② ２つの引数を受け取り、それらを足し合わせた結果を四捨五入した値を返す関数 plusRound を作成して正しく動くかテストしてください。
+
+const plusRound = function (x, y) {
+  return (x + y) * Math.round
+}
+
+//いや違うんかい、そういう使い方なのね
+const plusRound = function (x, y) {
+  return Math.round(x + y)
+}
